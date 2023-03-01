b5319037fcb5adad75b394c2e7e261a5a92047bf
Titov Dmitry <dm.a.titov@yandex.ru>
2023-03-01 17:23:53 +0300

    create window

--- main.cxx
+++ main.cxx
@@ -2,9 +2,25 @@
 // Created by asuka on 01.03.2023.
 //
 
+# include <GLFW/glfw3.h>
 # include <iostream>
 
 int main() {
-    std::cout << "hello, world\n";
+    if (!glfwInit()) {
+        std::cout << "failed init glfw\n";
+        return 1;
+    }
+
+    auto* window = glfwCreateWindow(900, 600, "OpenGL example", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout << "failed create window\n";
+        return 1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
